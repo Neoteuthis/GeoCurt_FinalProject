c6f1c72fdadda932aad60db525775df8d2e2a317
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-24 15:06:09 -0400

    added crystal script with health regen function

---
+++ CubeQuest/Assets/crystalscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystalscript : MonoBehaviour {
+    public float HP = 200;
+    public float MAXHP = 200;
+    public float regenrate = 1;
+    // Use this for initialization
+    void Start () {
+
+        }
+	
+	// Update is called once per frame
+	void Update () {
+        if (HP < MAXHP) {
+            //regenerate hp
+            HP += regenrate;
+        }
+	}
+}
