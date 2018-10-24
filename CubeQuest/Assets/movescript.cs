1653d67ebe93fa85ae07ce49abe53e717746a2e2
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-24 14:55:40 -0400

    MOVEMENT AND COLLISION SETUP

--- CubeQuest/Assets/movescript.cs
+++ CubeQuest/Assets/movescript.cs
@@ -7,6 +7,7 @@ public class movescript : MonoBehaviour {
     public bool isseekingcrystal;
     public bool isseekingplayer;
     public bool isrunning;
+    public float movespeed = 2;
 	// Use this for initialization
 	void Start () {
         
@@ -15,7 +16,7 @@ public class movescript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var X = gameObject.transform.position;
-        gameObject.transform.Translate(Random.Range(-0.1f,0.1f), Random.Range(-0.1f, 0.1f),0);
-
+        //gameObject.transform.Translate(Random.Range(-0.1f,0.1f), Random.Range(-0.1f, 0.1f),0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-movespeed, movespeed),Random.Range(-movespeed, movespeed));
     }
 }
---
+++ CubeQuest/Assets/playerscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour {
+    //vars
+    public int lives; //extras chances
+    public int HP; //character health
+    public int currentitem; //enum? each weapon type can have a number associated with it.
+    public float movespeed = 1000f; //speed multiplier applied to unit vector for motion
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //movement code
+            float LR = Input.GetAxisRaw("Horizontal");
+            float UD = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(LR* movespeed, 0) ;
+        }
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, UD* movespeed) ;
+        }
+        if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+}
