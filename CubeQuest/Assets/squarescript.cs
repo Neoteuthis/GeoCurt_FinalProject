a3f88b60a39c1a9fc10b52d7d3d618a4f42d59df
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-24 16:07:32 -0400

    added square coloring

---
+++ CubeQuest/Assets/squarescript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squarescript : MonoBehaviour {
+    public bool isred = true;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(isred == true)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(0, 0, 255, 100);
+        } else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 100);
+        }
+	}
+}
