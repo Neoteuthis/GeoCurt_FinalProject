718a31fc360c19045f52e1b9cab7f0977c8308a8
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-25 14:59:23 -0400

    allowed slimes to duplicate
    for some reason, summoned slimes are not visible, in the right spot or moving, but the are spawning

---
+++ CubeQuest/Assets/scripts/slimescript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slimescript : MonoBehaviour
+{
+    public int maxspawns = 1;
+    public int spawntime = 1200;
+    public int maxspawntime = 1200;
+    Vector3 currentpos;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        currentpos = gameObject.transform.position;
+        spawntime--;
+        if(spawntime < 0)
+        {
+            spawnNewSlime();
+            spawntime = maxspawntime;
+        }
+    }
+    //multiply
+    public void spawnNewSlime()
+    {
+        int spawnNum = maxspawns;
+        while (spawnNum >= 0)
+        {
+            GameObject slime = objectpooler.SharedInstance.GetPooledObject("Greenslime");
+            if (slime != null)
+            {
+                slime.transform.position =currentpos;
+                slime.SetActive(true);
+            }
+            spawnNum--;
+        }
+    }
+}
