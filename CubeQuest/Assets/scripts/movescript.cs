ada754458b406c5e4c16e3219dd9bcd02eb05d04
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-29 16:54:50 -0400

    added new enemy type

--- CubeQuest/Assets/scripts/movescript.cs
+++ CubeQuest/Assets/scripts/movescript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class movescript : MonoBehaviour {
     public bool isroaming;
+    public bool iswalking;
     public bool isseekingcrystal;
     public bool isseekingplayer;
     public bool isrunning;
     public float movespeed = 2;
+    public int changedir = 50;
+    public float xvel = 0;
+    public float yvel = 0;
     // Use this for initialization
     void Start() {
-
     }
 
     // Update is called once per frame
@@ -19,6 +22,43 @@ public class movescript : MonoBehaviour {
         //gameObject.transform.Translate(Random.Range(-0.1f,0.1f), Random.Range(-0.1f, 0.1f),0);
         if (isroaming) { 
              GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-movespeed, movespeed), Random.Range(-movespeed, movespeed));
+        }
+        if (iswalking)
+        {
+            changedir--;
+            if (changedir <= 0)
+            {
+                int ipswitch = Random.Range(0, 3);
+                switch (ipswitch){
+                    case 0:
+                        xvel = 0;
+                        yvel = movespeed;
+                        break;
+                    case 1:
+                        xvel = 0;
+                        yvel = -movespeed;
+                        break;
+                    case 2:
+                        xvel = movespeed;
+                        yvel = 0;
+                        break;
+                    case 3:
+                        xvel = -movespeed;
+                        yvel = 0;
+                        break;
+                    default:
+                        xvel = 0;
+                        yvel = movespeed;
+                        break;
+                }
+                changedir = 50;
+            }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xvel,yvel);
+            
+        }
+        if (isseekingplayer)
+        {
+
         }
     }
 }
